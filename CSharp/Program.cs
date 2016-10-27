fc6afe57246c051329aae67497008f264aded4f0
goto14 <elevatoraction@moog.raindrop.jp>
2016-10-27 22:51:47 +0900

    Template
    プロコン用C＃テンプレート

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.Linq;
 
-namespace ConsoleApplication
+public class Program
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+    public void Proc() {
+
+    }
+
+
+    public class Reader {
+        public static bool IsDebug = true;
+        private static System.IO.StringReader SReader;
+        private static string InitText = @"
+        
+put input text
+
+";
+        public static string ReadLine() {
+            if(IsDebug) {
+                if(SReader == null) {
+                    SReader = new System.IO.StringReader(InitText.Trim());
+                }
+                return SReader.ReadLine();
+            } else {
+                return Console.ReadLine();
+            }
         }
+    }
+    public static void Main(string[] args)
+    {
+        Program prg = new Program();
+        prg.Proc();
     }
 }
